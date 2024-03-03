f45594195b2c014f1dc7d54fbf7dd2f718dbd106
Lévon G.H. <126253100+lgukasyan@users.noreply.github.com>
2024-03-03 18:08:50 +0100

    feat: implement interface for env

--- cmd/serve.go
+++ cmd/serve.go
@@ -11,8 +11,8 @@ var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Start coupon service",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Get env
-		env := config.Env{}
+		// Create new env obj
+		env := config.New()
 
 		// Load env
 		if err := env.Load(); err != nil {
