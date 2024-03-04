f55145a5a332f5540212469953590e8923e10076
Lévon G.H. <126253100+lgukasyan@users.noreply.github.com>
2024-03-04 20:22:42 +0100

    test: add test for coupon.controller.create

--- internal/interface/controller/coupon_controller_test.go
+++ internal/interface/controller/coupon_controller_test.go
@@ -36,7 +36,7 @@ func TestCouponControllerCreateValidRequest(t *testing.T) {
 	mockService.AssertExpectations(t)
 }
 
-func TestCouponControllerCreateInvalidRequest(t *testing.T) {
+func TestCouponControllerCreateInvalidKeyName(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	r := gin.Default()
 
@@ -46,6 +46,27 @@ func TestCouponControllerCreateInvalidRequest(t *testing.T) {
 	r.POST("/coupon", controller.Create)
 
 	body := bytes.NewBufferString(`{"wrong": "12345", "discount": 1, "min_basket_value": 1}`)
+	req, err := http.NewRequest("POST", "/coupon", body)
+	assert.NoError(t, err, "error sending the GET request")
+	req.Header.Add("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	mockService.AssertExpectations(t)
+}
+
+func TestCouponControllerCreateInvalidString(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+
+	mockService := new(CouponServiceMock)
+
+	controller := NewCouponController(mockService)
+	r.POST("/coupon", controller.Create)
+
+	body := bytes.NewBufferString(``)
 	req, err := http.NewRequest("POST", "/coupon", body)
 	assert.NoError(t, err, "error sending the GET request")
 	req.Header.Add("Content-Type", "application/json")
